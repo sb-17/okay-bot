afbe30d99919863f81a0d0357127b59ee8cd3ffc
sb-17 <88972742+sb-17@users.noreply.github.com>
2022-02-02 22:22:25 +0100

    bot

--- index.py
+++ index.py
@@ -10,13 +10,9 @@ client = discord.Client()
 
 def get_database():
     from pymongo import MongoClient
-    import pymongo
 
     # Provide the mongodb atlas url to connect python to mongodb using pymongo
     CONNECTION_STRING = os.getenv("MONGOURI")
-
-    # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
-    from pymongo import MongoClient
     client = MongoClient(CONNECTION_STRING)
 
     # Create the database for our example (we will use the same database throughout the tutorial
